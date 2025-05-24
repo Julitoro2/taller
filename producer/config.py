d7c01ce2d1bc339595f49e5e74826cee59d2da2d
Julitoro2 <166272992+Julitoro2@users.noreply.github.com>
2025-05-23 23:48:03 -0500

    Delete producer directory

--- producer/config.py
+++
@@ -1,7 +0,0 @@
-RABBITMQ_HOST = "rabbitmq"
-RABBITMQ_PORT = 5672
-RABBITMQ_USER = "guest"
-RABBITMQ_PASS = "guest"
-EXCHANGE_NAME = "weather_exchange"
-ROUTING_KEY = "weather.log"
-QUEUE_NAME = "weather_queue"
--- producer/test_bulk_send.py
+++
@@ -1,5 +0,0 @@
-from data_generator import generate_bulk_data
-from producer import publish_message
-
-for msg in generate_bulk_data(100):
-    publish_message(msg)
